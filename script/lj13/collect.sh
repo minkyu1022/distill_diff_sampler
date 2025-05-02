f5769d2d49f848c51de2e3feec788735cabd36c1
kiyoung98 <kyseong98@kaist.ac.kr>
2025-05-03 01:28:17 +0900

    Update collect scripts for lj13 and lj55: comment out old commands and add new configurations with adjusted save directories

--- script/lj13/collect.sh
+++ script/lj13/collect.sh
@@ -1,11 +1,11 @@
-CUDA_VISIBLE_DEVICES=0 python src/collect.py \
-  --project lj13_mala \
-  --energy lj13 \
-  --teacher mala \
-  --burn_in 4000 \
-  --max_iter_ls 6000 \
-  --teacher_batch_size 500 \
-  --ld_schedule \
+# CUDA_VISIBLE_DEVICES=0 python src/collect.py \
+#   --project lj13_mala \
+#   --energy lj13 \
+#   --teacher mala \
+#   --burn_in 4000 \
+#   --max_iter_ls 6000 \
+#   --teacher_batch_size 500 \
+#   --ld_schedule \
 
 # CUDA_VISIBLE_DEVICES=0 python src/collect.py \
 #   --project lj13_mala \
@@ -16,3 +16,13 @@ CUDA_VISIBLE_DEVICES=0 python src/collect.py \
 #   --max_iter_ls 4000 \
 #   --teacher_batch_size 500 \
 #   --ld_schedule \
+
+CUDA_VISIBLE_DEVICES=0 python src/collect.py \
+  --project lj13_mala \
+  --energy lj13 \
+  --save_dir lj13_too_bad \
+  --teacher mala \
+  --burn_in 1000 \
+  --max_iter_ls 3000 \
+  --teacher_batch_size 500 \
+  --ld_schedule \
