474dc5ceb9fdb5e512418e9afa25857b40c8ea68
kiyoung98 <kyseong98@kaist.ac.kr>
2025-05-06 16:20:14 +0900

    lj13_teacher.sh 및 lj55_teacher.sh에서 각 스크립트 실행 후 sleep 명령 추가

--- lj13_teacher.sh
+++ lj13_teacher.sh
@@ -1,9 +1,16 @@
 bash script/lj13/ours.sh 0 0 "data/lj13_5K/mala" 12000 2000 1 &
+sleep 1
 bash script/lj13/ours.sh 1 0 "data/lj13_10K/mala" 12000 2000 2 &
+sleep 1
 bash script/lj13/ours.sh 2 0 "data/lj13_20K/mala" 12000 2000 4 &
+sleep 1
 bash script/lj13/ours.sh 3 0 "data/lj13_40K/mala" 12000 2000 8 &
+sleep 1
 bash script/lj13/ours.sh 4 0 "data/lj13_80K/mala" 12000 2000 16 &
+sleep 1
 bash script/lj13/ours.sh 5 0 "data/lj13_160K/mala" 12000 2000 32 &
+sleep 1
 bash script/lj13/ours.sh 6 0 "data/lj13_320K/mala" 12000 2000 64 &
+sleep 1
 bash script/lj13/ours.sh 7 0 "data/lj13_640K/mala" 12000 2000 128 &
 wait
--- lj55_teacher.sh
+++ lj55_teacher.sh
@@ -1,9 +1,16 @@
 bash script/lj55/ours.sh 0 0 "data/lj55_5K/mala" 20000 10000 1 &
+sleep 1
 bash script/lj55/ours.sh 1 0 "data/lj55_10K/mala" 20000 10000 2 &
+sleep 1
 bash script/lj55/ours.sh 2 0 "data/lj55_20K/mala" 20000 10000 4 &
+sleep 1
 bash script/lj55/ours.sh 3 0 "data/lj55_40K/mala" 20000 10000 8 &
+sleep 1
 bash script/lj55/ours.sh 4 0 "data/lj55_80K/mala" 20000 10000 16 &
+sleep 1
 bash script/lj55/ours.sh 5 0 "data/lj55_160K/mala" 20000 10000 32 &
+sleep 1
 bash script/lj55/ours.sh 6 0 "data/lj55_320K/mala" 20000 10000 64 &
+sleep 1
 bash script/lj55/ours.sh 7 0 "data/lj55_640K/mala" 20000 10000 128 &
 wait
