5ccbfd631680c12a85f164f0a441d4cdb6edcb55
kiyoung98 <kyseong98@kaist.ac.kr>
2025-05-06 21:22:54 +0900

    lj13.sh 및 lj55.sh에서 스크립트 호출을 수정하여 실행 순서 및 파라미터를 조정

--- lj55.sh
+++ lj55.sh
@@ -1,17 +1,20 @@
 bash script/lj55/ours.sh 0 0 &
 sleep 1
-bash script/lj55/ours.sh 1 1 &
+bash script/lj55/ours.sh 0 1 &
 sleep 1
-bash script/lj55/pis_lp.sh 2 0 &
+bash script/lj55/tb.sh 1 0 &
 sleep 1
-bash script/lj55/pis_lp.sh 3 1 &
+bash script/lj55/tb.sh 1 1 &
 sleep 1
-bash script/lj55/subtb_lp.sh 4 0 &
+bash script/lj55/pis.sh 2 0 &
 sleep 1
-bash script/lj55/subtb_lp.sh 5 1 &
+bash script/lj55/pis.sh 2 1 &
 sleep 1
-bash script/lj55/tb_ls.sh 6 0 &
+bash script/lj55/tb_ls.sh 3 0 &
 sleep 1
-bash script/lj55/tb_ls.sh 7 1 &
+bash script/lj55/tb_ls.sh 3 1 &
 sleep 1
+bash script/lj55/tb_expl_ls.sh 4 0 &
+sleep 1
+bash script/lj55/tb_expl_ls.sh 4 1 &
 wait
